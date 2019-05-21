1d37311cd7d0fed34a759737833850d977ef4db5
Adrian Sitko <foresai221@gmail.com>
2019-05-21 16:13:32 +0200

    better look

--- src/App.js
+++ src/App.js
@@ -1,78 +1,147 @@
-import React from 'react';
-import './App.css';
-import Days from './Days';
-export default class App extends React.Component {
+import React from "react";
+import "./App.css";
+import Days from "./Days";
 
+export default class App extends React.Component {
   state = {
     isLoading: true,
     weather: [],
-    dateChosen: 0,
+    dateChosen: 0
+  };
+
+  componentDidMount() {
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/ce8ba041d29f48faad3399be62400610/50.0125,%2020.988611?units=si"
+    ).then(response => {
+      response.json().then(data => {
+        this.setState({ weather: data, isLoading: false });
+      });
+    });
   }
 
-  componentDidMount(){
-    fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/ce8ba041d29f48faad3399be62400610/50.0125,%2020.988611?units=si').then(response => {
-      response.json().then((data)=>{
-        this.setState({weather: data, isLoading: false});
-      })
-    })
-
-
-    
-  }
-
-  getWeekDay(date){
-    let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  getWeekDay(date) {
+    let weekdays = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
     let day = date.getDay();
     return weekdays[day];
   }
 
-  getMonthName(date){
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];  
+  getMonthName(date) {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
     let month = date.getMonth();
     return months[month];
   }
 
   render() {
-    if(this.state.isLoading){
-      return(
+    if (this.state.isLoading) {
+      return (
         <div id="App">
           <h1> Please wait while we load data </h1>
         </div>
-      )
-    } 
+      );
+    }
 
     let days = [];
     let tmp = this.state.weather.daily.data;
 
-    Object.keys(tmp).forEach(day=>{
-      days.push(<Days onClick={() => this.setState({dateChosen: day})} key={day} icon={tmp[day].icon} date={new Date(tmp[day].time*1000)} tempMax={tmp[day].temperatureMax} tempMin={tmp[day].temperatureMin}/>)
-    })
+    Object.keys(tmp).forEach(day => {
+      days.push(
+        <Days
+          onClick={() => this.setState({ dateChosen: day })}
+          key={day}
+          icon={tmp[day].icon}
+          date={new Date(tmp[day].time * 1000)}
+          tempMax={tmp[day].temperatureMax}
+          tempMin={tmp[day].temperatureMin}
+        />
+      );
+    });
 
-    let chosen = {
-      date: 0,
-      temp: "",
-      icon: "",
-    }
+    let chosen = {};
 
-    if(this.state.dateChosen==0){
-      chosen.date = new Date(this.state.weather.currently.time*1000);
-      chosen.temp = "Current: "+this.state.weather.currently.temperature+ "°C";
+    chosen.sunrise =
+      "Sunrise at: " +
+      new Date(tmp[this.state.dateChosen].sunriseTime * 1000).getHours() +
+      ":" +
+      new Date(tmp[this.state.dateChosen].sunriseTime * 1000).getMinutes();
+
+    if (this.state.dateChosen == 0) {
+      chosen.date = new Date(this.state.weather.currently.time * 1000);
+      chosen.temp =
+        "Current: " + this.state.weather.currently.temperature + " [°C]";
+      chosen.wind =
+        "Wind speed: " + this.state.weather.currently.windSpeed + " [m/s]";
+      chosen.humidity =
+        "Humidity: " + this.state.weather.currently.humidity * 100 + " [%]";
       chosen.icon = this.state.weather.currently.icon;
     } else {
-      chosen.date = new Date(tmp[this.state.dateChosen].time*1000);
-      chosen.temp = "Average: "+Math.round((tmp[this.state.dateChosen].temperatureMax+tmp[this.state.dateChosen].temperatureMin)/2) + "°C";
+      chosen.date = new Date(tmp[this.state.dateChosen].time * 1000);
+      chosen.temp =
+        "Average: " +
+        Math.round(
+          (tmp[this.state.dateChosen].temperatureMax +
+            tmp[this.state.dateChosen].temperatureMin) /
+            2
+        ) +
+        " [°C]";
+      chosen.wind =
+        "Wind speed: " + tmp[this.state.dateChosen].windSpeed + " [m/s]";
+      chosen.humidity =
+        "Humidity: " + tmp[this.state.dateChosen].humidity * 100 + " [%]";
+
       chosen.icon = tmp[this.state.dateChosen].icon;
     }
 
-  return (
-    <div className="App">
-      <div id="container">{days}</div>
-      <div id="extended" >
-        <h1>{this.getWeekDay(chosen.date) +" "+ chosen.date.getDate() +" "+ this.getMonthName(chosen.date)}</h1>
-        <h1>{chosen.temp}</h1>
-        <img src={require("./icons/"+chosen.icon+".svg")} style={{width: 200, height: 200}} alt={this.props.icon} />
+    return (
+      <div className="App">
+        <div id="header">
+          <div id="top">
+            <img
+              src={require("./icons-white/" + chosen.icon + ".svg")}
+              id="icon-big"
+              alt={this.props.icon}
+            />
+            <div id="text">
+              <h1 className="item">{chosen.temp}</h1>
+              <h1 className="item">
+                {this.getWeekDay(chosen.date) +
+                  " " +
+                  chosen.date.getDate() +
+                  " " +
+                  this.getMonthName(chosen.date)}
+              </h1>
+            </div>
+          </div>
+          <div id="bot">
+            <div id="text">
+              <p className="item">{chosen.wind}</p>{" "}
+              <p className="item">{chosen.humidity}</p>{" "}
+              <p className="item">{chosen.sunrise}</p>
+            </div>
+          </div>
+        </div>
+        <div id="content">{days}</div>
       </div>
-    </div>
-  );
+    );
   }
 }
--- src/Days.js
+++ src/Days.js
@@ -3,62 +3,6 @@ import './Days.css';
 
 export default class Days extends Component {
 
-    state = {
-        red: 0,
-        green: 0,
-        blue: 0,
-    }
-
-    componentDidMount(){
-        let red, green, blue = 0;
-            switch(this.props.icon){
-                case "clear-day":
-                    red = 255;
-                    green = 255;
-                    blue = 153;
-                    break;
-                case "clear-night":
-                    red = 255;
-                    green = 255;
-                    blue = 153;
-                    break;
-                case "wind":
-                    red = 206;
-                    green = 222;
-                    blue = 244;
-                    break;   
-                case "rain":
-                    red = 169;
-                    green = 197;
-                    blue = 235;
-                    break; 
-                case "partly-cloudy-night":
-                    red = 194;
-                    green = 105;
-                    blue = 254;
-                    break; 
-                case "partly-cloudy-day":
-                    red = 209;
-                    green = 255;
-                    blue = 179;
-                    break;  
-                case "cloudy":
-                    red = 209;
-                    green= 255;
-                    blue= 179;
-                    break;  
-                default: 
-                    break; 
-            } 
-        this.setState({red: this.getRandomInt(red-15, red+10), green: this.getRandomInt(green-15, green+10), blue: this.getRandomInt(blue-15, blue+10)});
-    }
-
-    getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
     getWeekDay(date){
         let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
         let day = date.getDay();
@@ -73,13 +17,18 @@ export default class Days extends Component {
 
   render() {
     return (
-      <div className="box" onClick={this.props.onClick} style={{backgroundColor: "rgb("+this.state.red+","+this.state.green+","+this.state.blue+")"}}>
-        <h2>{this.getWeekDay(this.props.date)}</h2>
-        <h3>{this.props.date.getDate()}</h3>
-        <h3>{this.getMonthName(this.props.date)}</h3>
-        <h2>{"Max: " + Math.round(this.props.tempMax) + "°C"}</h2>
-        <h2>{"Min: " + Math.round(this.props.tempMin) + "°C"}</h2>
-        <img src={require("./icons/"+this.props.icon+".svg")} alt={this.props.icon} />
+      <div className="box" onClick={this.props.onClick} >
+
+        <p className="part">{this.getWeekDay(this.props.date)}</p>
+
+
+        <img  className="part" src={require("./icons/"+this.props.icon+".svg")} alt={this.props.icon} />
+
+        <div className="part">
+        <p style={{fontSize: 16}}>{Math.round(this.props.tempMax) + "°C"}</p>
+        <p style={{fontSize: 12, color: "rgb(100,100,100)"}}>{Math.round(this.props.tempMin) + "°C"}</p>
+        </div>
+
       </div>
     )
   }
