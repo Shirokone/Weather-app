79e7814cdc3d3da810aee4978d4a937fbd35400b
Adrian Sitko <foresai221@gmail.com>
2019-05-21 16:28:27 +0200

    Looks

--- src/Days.js
+++ src/Days.js
@@ -1,35 +1,52 @@
-import React, { Component } from 'react';
-import './Days.css';
+import React, { Component } from "react";
+import "./Days.css";
 
 export default class Days extends Component {
+  getWeekDay(date) {
+    let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = date.getDay();
+    return weekdays[day];
+  }
 
-    getWeekDay(date){
-        let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-        let day = date.getDay();
-        return weekdays[day];
-    }
-
-    getMonthName(date){
-        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];  
-        let month = date.getMonth();
-        return months[month];
-    }
+  getMonthName(date) {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    let month = date.getMonth();
+    return months[month];
+  }
 
   render() {
     return (
-      <div className="box" onClick={this.props.onClick} >
-
+      <div className="box" onClick={this.props.onClick}>
         <p className="part">{this.getWeekDay(this.props.date)}</p>
 
-
-        <img  className="part" src={require("./icons/"+this.props.icon+".svg")} alt={this.props.icon} />
+        <img
+          className="part"
+          src={require("./icons/" + this.props.icon + ".svg")}
+          alt={this.props.icon}
+        />
 
         <div className="part">
-        <p style={{fontSize: 16}}>{Math.round(this.props.tempMax) + "°C"}</p>
-        <p style={{fontSize: 12, color: "rgb(100,100,100)"}}>{Math.round(this.props.tempMin) + "°C"}</p>
+          <p style={{ fontSize: 16 }}>
+            {Math.round(this.props.tempMax) + "°C"}
+          </p>
+          <p style={{ fontSize: 12, color: "rgb(100,100,100)" }}>
+            {Math.round(this.props.tempMin) + "°C"}
+          </p>
         </div>
-
       </div>
-    )
+    );
   }
 }
