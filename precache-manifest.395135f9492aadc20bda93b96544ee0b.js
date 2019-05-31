8dc40e1aff885d7ffe8582a56861d14ad9f3e31c
Adrian Sitko <foresai221@gmail.com>
2019-05-31 21:23:04 +0200

    Updates

---
+++ precache-manifest.395135f9492aadc20bda93b96544ee0b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b56173af1668194bf3f6d8bb4f1356d",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "84fb5a8c4f3a78cd67c9",
+    "url": "/weather/static/css/main.64c7ca6c.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/weather/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "84fb5a8c4f3a78cd67c9",
+    "url": "/weather/static/js/main.c105bd68.chunk.js"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  },
+  {
+    "revision": "53f709c56c0cb268362ba119c34e072a",
+    "url": "/weather/static/media/clear-day.53f709c5.svg"
+  },
+  {
+    "revision": "973b03530e1157f460b55ec349ffe40e",
+    "url": "/weather/static/media/clear-day.973b0353.svg"
+  },
+  {
+    "revision": "7b6d6c0772cc59b7b5c089fff8315ee2",
+    "url": "/weather/static/media/clear-night.7b6d6c07.svg"
+  },
+  {
+    "revision": "b87ca8b626b1d792e4b35da43f795ba7",
+    "url": "/weather/static/media/clear-night.b87ca8b6.svg"
+  },
+  {
+    "revision": "0f32323e76ed110541cc2658c2ab235c",
+    "url": "/weather/static/media/cloudy.0f32323e.svg"
+  },
+  {
+    "revision": "79b3c647bcb4502fe1f95e9d9c5de5d9",
+    "url": "/weather/static/media/cloudy.79b3c647.svg"
+  },
+  {
+    "revision": "3694ef67610075498c91cdb7b2bfb2f4",
+    "url": "/weather/static/media/fog.3694ef67.svg"
+  },
+  {
+    "revision": "fcd8705ff785e7b00a041e44c335e543",
+    "url": "/weather/static/media/fog.fcd8705f.svg"
+  },
+  {
+    "revision": "534afba6dc879efc03f27d5c964416bf",
+    "url": "/weather/static/media/partly-cloudy-day.534afba6.svg"
+  },
+  {
+    "revision": "94e4dfb63686d41118eb73590066ee8a",
+    "url": "/weather/static/media/partly-cloudy-day.94e4dfb6.svg"
+  },
+  {
+    "revision": "675a7cddcf441a98bad0880208ecdb37",
+    "url": "/weather/static/media/partly-cloudy-night.675a7cdd.svg"
+  },
+  {
+    "revision": "bd9057d02bc2845783e6e1cd28213469",
+    "url": "/weather/static/media/partly-cloudy-night.bd9057d0.svg"
+  },
+  {
+    "revision": "316ec3310f84545f10f9525b44509d10",
+    "url": "/weather/static/media/rain.316ec331.svg"
+  },
+  {
+    "revision": "7a409f4074648138a0b376aa192b28c8",
+    "url": "/weather/static/media/rain.7a409f40.svg"
+  },
+  {
+    "revision": "1c0e13945710754cce9441e06a7e618b",
+    "url": "/weather/static/media/sleet.1c0e1394.svg"
+  },
+  {
+    "revision": "20379c89e064dca7173f49395ec16223",
+    "url": "/weather/static/media/sleet.20379c89.svg"
+  },
+  {
+    "revision": "9caa25348402f76ef5507e4d8210a797",
+    "url": "/weather/static/media/snow.9caa2534.svg"
+  },
+  {
+    "revision": "eace860f9832be6116f644968bcdc121",
+    "url": "/weather/static/media/snow.eace860f.svg"
+  },
+  {
+    "revision": "6b4d10062fb150d93eb6d0a550d19947",
+    "url": "/weather/static/media/wind.6b4d1006.svg"
+  },
+  {
+    "revision": "c929d412dcf7e84f6b4d45f79ab09ed9",
+    "url": "/weather/static/media/wind.c929d412.svg"
+  }
+]);
